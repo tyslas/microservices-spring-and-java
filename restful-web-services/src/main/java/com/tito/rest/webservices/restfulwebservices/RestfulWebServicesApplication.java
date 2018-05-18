d6df7528bef940288c35fdbb014d9bf933169b97
Tito Yslas <roberto.yslas@gmail.com>
2018-05-18 10:31:29 -0600

    implemented a simpler approach to internationalization with less code

--- restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -18,15 +19,8 @@ public class RestfulWebServicesApplication {
 
 	@Bean
   public LocaleResolver localeResolver() {
-    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
     localeResolver.setDefaultLocale(Locale.US);
     return localeResolver;
-  }
-
-  @Bean
-  public ResourceBundleMessageSource messageSource() {
-	  ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	  messageSource.setBasename("messages");
-	  return messageSource;
   }
 }
--- restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.tito.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -40,8 +41,8 @@ public class HelloWorldController {
   }
 
   @GetMapping(path="/hello-world-internationalized")
-  public String helloWorldInt(@RequestHeader(name  = "Accept-Language", required = false) Locale locale) {
-    return messageSource.getMessage("good.morning.message", null, locale);
+  public String helloWorldInternationalized(Locale locale) {
+    return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
   }
 
 }

f628789d75818848e3b76911cf5fa7eabcef8d52
Tito Yslas <roberto.yslas@gmail.com>
2018-05-21 10:01:09 -0600

    enhanced Swagger documentation with custom annotations

--- restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -2,18 +2,34 @@ package com.tito.rest.webservices.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration //Config file
 @EnableSwagger2 //enable swagger
 public class SwaggerConfig {
+
+  public static final Contact DEFAULT_CONTACT = new Contact("Fred Flintstone", "fred.feet", "fred@flintstone.com");
+  private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+      "Awesome API Title", "Awesome API Description", "1.1",
+      "wwww.termsofservice.url", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+  private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
   @Bean //define a @Bean => Docket
   public Docket api() {
     //designate that the Docket bean be configured to use Swagger
     //all the paths
     //all the apis
-    return new Docket(DocumentationType.SWAGGER_2);
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(DEFAULT_API_INFO)
+        .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+        .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
   }
 }
--- restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/tito/rest/webservices/restfulwebservices/user/User.java
@@ -1,14 +1,22 @@
 package com.tito.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "all details about the user")
 public class User {
   private Integer id;
+
   @Size(min=2, message = "name should have at least 2 characters")
+  @ApiModelProperty(notes = "name should have at least 2 characters")
   private String name;
+
   @Past
+  @ApiModelProperty(notes = "birth should be previous to the current time of selection")
   private Date birthDate;
 
   protected User() {

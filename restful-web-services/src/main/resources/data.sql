88c3952818370a2f923f6cf9d5daf0f7d70f09db
Tito Yslas <roberto.yslas@gmail.com>
2018-05-23 10:35:55 -0600

    SQL reserved words => .toUpperCase()

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into user values(10001, sysdate(), 'Brian')
-insert into user values(10002, sysdate(), 'Joy')
-insert into user values(10003, sysdate(), 'Jill')
+INSERT INTO user VALUES(10001, sysdate(), 'Brian')
+INSERT INTO user VALUES(10002, sysdate(), 'Joy')
+INSERT INTO user VALUES(10003, sysdate(), 'Jill')
 
-insert into post values(11001, 'my first post', 10001)
-insert into post values(11002, 'my second post', 10001)
+INSERT INTO post VALUES(11001, 'my first post', 10001)
+INSERT INTO post VALUES(11002, 'my second post', 10001)

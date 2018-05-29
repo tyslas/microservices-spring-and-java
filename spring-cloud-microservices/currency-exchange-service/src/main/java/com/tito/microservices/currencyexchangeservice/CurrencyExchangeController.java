90095e3f28d5c8f9420d7c763ad91a0e289282c3
Tito Yslas <roberto.yslas@gmail.com>
2018-05-29 14:16:57 -0600

    set up dynamic port assignment that is returned at the endpoint response

--- spring-cloud-microservices/currency-exchange-service/src/main/java/com/tito/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ spring-cloud-microservices/currency-exchange-service/src/main/java/com/tito/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.tito.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,15 @@ import java.math.BigDecimal;
 @RestController //allows us to accept JSON requests and send JSON back out
 public class CurrencyExchangeController {
 
+  @Autowired
+  private Environment environment;
+
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-    return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65)); //return hard coded bean for exchange value
+    ExchangeValue exchangeValue =
+        new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65)); //return hard coded bean for exchange value
+    exchangeValue
+        .setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+    return exchangeValue;
   }
 }
--- spring-cloud-microservices/currency-exchange-service/src/main/java/com/tito/microservices/currencyexchangeservice/ExchangeValue.java
+++ spring-cloud-microservices/currency-exchange-service/src/main/java/com/tito/microservices/currencyexchangeservice/ExchangeValue.java
@@ -7,6 +7,7 @@ public class ExchangeValue {
   private String from;
   private String to;
   private BigDecimal conversionMultiple;
+  private int port;
 
   public ExchangeValue() {
   }
@@ -32,5 +33,13 @@ public class ExchangeValue {
 
   public BigDecimal getConversionMultiple() {
     return conversionMultiple;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
   }
 }

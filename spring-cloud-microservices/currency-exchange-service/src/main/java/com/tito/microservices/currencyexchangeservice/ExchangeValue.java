b147561c467c65d8fefcb76490c83468a0fe675d
Tito Yslas <roberto.yslas@gmail.com>
2018-05-30 09:54:47 -0600

    configured JPA and initialized data

--- spring-cloud-microservices/currency-exchange-service/src/main/java/com/tito/microservices/currencyexchangeservice/ExchangeValue.java
+++ spring-cloud-microservices/currency-exchange-service/src/main/java/com/tito/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,10 +1,20 @@
 package com.tito.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
+
+  @Id
   private Long id;
+
+  @Column(name = "currency_from")
   private String from;
+
+  @Column(name = "currency_to")
   private String to;
   private BigDecimal conversionMultiple;
   private int port;
---
+++ spring-cloud-microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+VALUES(10001, 'USD', 'INR', 65, 0);
+INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+VALUES(10002, 'EUR', 'INR', 75, 0);
+INSERT INTO exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+VALUES(10003, 'AUD', 'INR', 25, 0);

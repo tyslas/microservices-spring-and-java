83e3ebaeb331526cdf234d7764bd09759fd627c9
Tito Yslas <roberto.yslas@gmail.com>
2018-05-30 15:55:47 -0600

    Currency Exchange microservices now invokes that Currency Conversion microservice

--- spring-cloud-microservices/currency-conversion-service/src/main/java/com/tito/microservices/currencyconversionservice/CurrencyConversionController.java
+++ spring-cloud-microservices/currency-conversion-service/src/main/java/com/tito/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,14 @@
 package com.tito.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -12,6 +16,24 @@ public class CurrencyConversionController {
   @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionBean convertCurrency(
       @PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-    return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+
+    Map<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
+
+    ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+        "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+        CurrencyConversionBean.class,
+        uriVariables);
+
+    CurrencyConversionBean response = responseEntity.getBody();
+    return new CurrencyConversionBean(
+        response.getId(),
+        from,
+        to,
+        response.getConversionMultiple(),
+        quantity,
+        quantity.multiply(response.getConversionMultiple()),
+        response.getPort());
   }
 }

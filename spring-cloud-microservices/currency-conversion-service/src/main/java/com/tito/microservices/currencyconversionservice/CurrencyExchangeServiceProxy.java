b284834acd28d1a10113fdc52ff027c1df96784a
Tito Yslas <roberto.yslas@gmail.com>
2018-05-31 10:55:13 -0600

    set up client side load balancing with Ribbon

--- spring-cloud-microservices/currency-conversion-service/src/main/java/com/tito/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ spring-cloud-microservices/currency-conversion-service/src/main/java/com/tito/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,13 @@
 package com.tito.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service") //when using Ribbon w/Feign we don't need the url
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")

5e0e6b5e399c3df7aec830a2fb25c02068dddaa1
Tito Yslas <roberto.yslas@gmail.com>
2018-05-31 11:48:37 -0600

    connected currency conversion microservice to Eureka

--- spring-cloud-microservices/currency-conversion-service/src/main/java/com/tito/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ spring-cloud-microservices/currency-conversion-service/src/main/java/com/tito/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.tito.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.tito.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
